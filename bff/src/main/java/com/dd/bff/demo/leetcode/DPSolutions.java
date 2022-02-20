8962d3e604d566a9e290eb9b7a305cd2ed8ee235
brycedd <trcat@outlook.com>
2022-02-20 21:49:28 +0800

    leetCode

--- bff/src/main/java/com/dd/bff/demo/leetcode/DPSolutions.java
+++ bff/src/main/java/com/dd/bff/demo/leetcode/DPSolutions.java
@@ -7,14 +7,15 @@ package com.dd.bff.demo.leetcode;
 public class DPSolutions {
 
     private static final int jumpParam = 4;
-    private static final int[] lengthOfLISParam = {10,9,2,5,3,7,101,18};
+    private static final int[] lengthOfLISParam = {10, 9, 2, 5, 3, 7, 101, 18};
 
     public static void main(String[] args) {
+        System.out.println("D688+++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(D688.knightProbability(3,2,0,0));
         System.out.println("D300+++++++++++++++++++++++++++++++++++++++++");
         System.out.println(D300.lengthOfLIS(lengthOfLISParam));
         System.out.println("DO10+++++++++++++++++++++++++++++++++++++++++");
         System.out.println(DO10.jump(jumpParam));
-
     }
 
     /**
@@ -70,7 +71,51 @@ public class DPSolutions {
         }
     }
 
+    /**
+     * <strong>688. 骑士在棋盘上的概率</strong><br/>
+     * 在一个n x n的国际象棋棋盘上，一个骑士从单元格 (row, column开始，并尝试进行 k 次移动。<br/>
+     * 行和列是 从 0 开始 的，所以左上单元格是 (0,0) ，右下单元格是 (n - 1, n - 1) 。<br/>
+     * 象棋骑士有8种可能的走法，如下图所示。每次移动在基本方向上是两个单元格，然后在正交方向上是一个单元格。<br/>
+     * 每次骑士要移动时，它都会随机从8种可能的移动中选择一种(即使棋子会离开棋盘)，然后移动到那里。<br/>
+     * 骑士继续移动，直到它走了 k 步或离开了棋盘。<br/>
+     * 返回 骑士在棋盘停止移动后仍留在棋盘上的概率 。<br/>
+     * <strong>题解:</strong><br/>
+     * <p>
+     * f[i][j][p]=∑f[nx][ny][p−1]/8
+     */
     static class D688 {
 
+        // 每一次移动可能出现的x，y轴变化可能性
+        private static final int[][] dirs = new int[][]{{-1, -2}, {-1, 2}, {1, 2}, {1, -2}, {-2, -1}, {-2, 1}, {2, -1}, {2, 1}};
+
+        public static double knightProbability(int n, int k, int row, int column) {
+            // 有可能走0步，所以此处为k+1；
+            double[][][] dp = new double[n][n][k + 1];
+            // 初始化,先为走0步赋初始值
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dp[i][j][0] = 1;
+                }
+            }
+            // 开始从第一步循环，找出在棋盘内的每一步
+            for (int p = 1; p <= k; p++) {
+                for (int x = 0; x < n; x++) {
+                    for (int y = 0; y < n; y++) {
+                        for (int[] dir : dirs) {
+                            int nx = x + dir[0];
+                            int ny = y + dir[1];
+                            if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                                // 落在了棋盘外,此处的概率作废
+                                continue;
+                            }
+                            // 落在棋盘内,讲所有可能性做加和；
+                            // 此处概率等于走到某个下一步 反推走回来的概率；
+                            dp[x][y][p] += dp[nx][ny][p - 1] / 8;
+                        }
+                    }
+                }
+            }
+            return dp[row][column][k];
+        }
     }
 }
