c9a74dbe7eab1654670c5f87764aea42bf613872
brycedd <trcat@outlook.com>
2023-07-12 17:12:34 +0800

    添加demo

---
+++ bff/src/main/java/com/dd/bff/demo/thread/springtxdemo/TransactionManager.java
@@ -0,0 +1,40 @@
+package com.dd.bff.demo.thread.springtxdemo;
+
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+/**
+ * @author Bryce_dd 2023/7/11 19:27
+ */
+
+public class TransactionManager {
+    private static final ThreadLocal<Transaction> transactionHolder = new ThreadLocal<>();
+
+    public static void beginTransaction() {
+        Transaction transaction = new Transaction();
+        transaction.start();
+        transactionHolder.set(transaction);
+        TransactionSynchronizationManager.initSynchronization();
+    }
+
+    public static void commitTransaction() {
+        Transaction transaction = transactionHolder.get();
+        transaction.commit();
+        transactionHolder.remove();
+        TransactionSynchronizationManager.clearSynchronization();
+    }
+
+    public static void rollbackTransaction() {
+        Transaction transaction = transactionHolder.get();
+        transaction.rollback();
+        transactionHolder.remove();
+        TransactionSynchronizationManager.clearSynchronization();
+    }
+
+    public static Transaction getCurrentTransaction() {
+        return transactionHolder.get();
+    }
+}
+
+
+
+
